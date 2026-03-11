3d9003a095cc1ea4a3ef3bd5f724a6a42ca91bde
Hemanth Kumar <hemanth@example.com>
2026-03-11 12:37:34 +0530

    app updated

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Docker Container Application with CI/CD Pipeline Running Successfully!\n");
+  res.end("Hemanth Docker Container Application with CI/CD Pipeline Running Successfully!\n");
 });
 
 server.listen(port, hostname, () => {
